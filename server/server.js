63569f526437b3f43d8b48658118f6b4991379ad
lakhan sharan <lakhansharan946@gmail.com>
2025-07-05 19:19:33 +0530

    fixed not found problem app is fully in working conditin

--- server/server.js
+++ server/server.js
@@ -1,23 +1,39 @@
 import express from "express";
 import dotenv from "dotenv";
-dotenv.config();
-import orderRoutes from "./routes/orderRoutes.js";
+import path from "path";
 import cors from "cors";
+
 import connectDB from "./config/db.js";
+import orderRoutes from "./routes/orderRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import adminRoutes from "./routes/adminRoutes.js";
 
+// Load env variables
+dotenv.config();
+
 // Connect to MongoDB
 connectDB();
+
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-// Routes
+// API Routes
 app.use("/api/products", productRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/admin", adminRoutes);
+
+// Serve frontend for production
+const __dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/dist")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.join(__dirname, "client", "dist", "index.html"))
+  );
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
   console.log(`🚀 Server running on http://localhost:${PORT}`)
