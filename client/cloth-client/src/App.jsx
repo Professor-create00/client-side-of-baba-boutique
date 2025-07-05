4e11edab8de2843888bf2c1a06d04dae758cee28
lakhan sharan <lakhansharan946@gmail.com>
2025-07-05 17:51:58 +0530

    redesign of frontend

--- client/cloth-client/src/App.jsx
+++ client/cloth-client/src/App.jsx
@@ -1,37 +1,38 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AddProduct from "./pages/admin/AddProduct";
+import AdminProducts from "./pages/admin/AdminProducts";
+import {   Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import ProductDetailPage from "./pages/ProductDetails";
+import AdminOrders from "./pages/admin/AdminOrders";
 import CategoryPage from "./pages/CategoryPage";
-import AdminDashboard from "./pages/AdminDashboard";
+import CartPage from "./pages/CartPage";
+import PrivateRoute from "./utils/PrivateRoute";
+import EditProduct from "./pages/admin/EditProduct";
 import AdminLogin from "./pages/AdminLogin";
-import AddProduct from "./pages/AddProduct";
-import EditProduct from "./pages/EditProduct";
-import ProductDetails from "./pages/ProductDetails";
-import AdminOrders from "./pages/AdminOrders";
-
-function App() {
+const App = () => {
   return (
-    <Router>
-      <Navbar />
+    <>
+    <Navbar />
+    
       <Routes>
-        {/* Public Routes */}
         <Route path="/" element={<Home />} />
-        <Route path="/saree" element={<CategoryPage category="saree" />} />
-        <Route path="/salwar-kurti" element={<CategoryPage category="salwar-kurti" />} />
-        <Route path="/night-dress" element={<CategoryPage category="night-dress" />} />
-        <Route path="/pickle" element={<CategoryPage category="pickle" />} />
-        <Route path="/organic-masalas" element={<CategoryPage category="organic-masalas" />} />
-        <Route path="/product/:id" element={<ProductDetails />} />
-
-        {/* Admin Routes */}
+        <Route path="/category/:categoryName" element={<CategoryPage/>} />
+        <Route path="/product/:id" element={<ProductDetailPage />} />
+        <Route path="/cart" element={<CartPage />} />
         <Route path="/admin/login" element={<AdminLogin />} />
-        <Route path="/admin/dashboard" element={<AdminDashboard />} />
-        <Route path="/admin/add-product" element={<AddProduct />} />
-        <Route path="/admin/edit-product/:id" element={<EditProduct />} />
-        <Route path="/admin/orders" element={<AdminOrders />} />
+        <Route path="/admin/products" element={
+          <PrivateRoute><AdminProducts /></PrivateRoute>} />
+        <Route path="/admin/products/add" element={
+          <PrivateRoute><AddProduct /></PrivateRoute>} />
+        <Route path="/admin/orders" element={
+          <PrivateRoute><AdminOrders /></PrivateRoute>} />
+        <Route path="/admin/products/edit/:id" element={
+          <PrivateRoute>< EditProduct/></PrivateRoute>} />
       </Routes>
-    </Router>
+  
+    </>
   );
-}
+};
 
 export default App;
